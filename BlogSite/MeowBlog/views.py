4381e3ed568176fde5f3ddf20d523e2b62af0980
millennumparade <liuyl2022@gmail.com>
2021-01-24 23:09:49 -0500

    init view functions

--- BlogSite/MeowBlog/views.py
+++ BlogSite/MeowBlog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
 
-# Create your views here.
+@require_POST()
+def createUser(request):
+    ...
+
+
+def deleteUser(request):
+    ...
+
+
+def updateUser(request):
+    ...
+
+@require_GET()
+def readUser(request):
+    ...
+
